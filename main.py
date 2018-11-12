a0fbd55a8b480f89178312dda3bec5e445f294e4
meirelon <nestelm@gmail.com>
2018-11-12 13:46:43 -0500

    changed to isinstance

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ def webhook(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-        if update is None:
+        if isinstance(update, None):
             return "not ok"
-
-        chat_id = update.message.chat.id
-        if update.message.text.lower() == "what is my name?":
-            say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
-            bot.sendMessage(chat_id=chat_id, text=say_hello_username)
         else:
-            bot.sendMessage(chat_id=chat_id, text=update.message.text)
-            return "ok"
+            chat_id = update.message.chat.id
+            if update.message.text.lower() == "what is my name?":
+                say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
+                bot.sendMessage(chat_id=chat_id, text=say_hello_username)
+            else:
+                bot.sendMessage(chat_id=chat_id, text=update.message.text)
+                return "ok"

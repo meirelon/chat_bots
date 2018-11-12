8d3fb0daa3229b89447891cd33f8e855da36ffc0
meirelon <nestelm@gmail.com>
2018-11-12 12:08:01 -0500

    reseting bot

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ def webhook(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
-        # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
-        # bot.sendMessage(chat_id=chat_id, text=say_hello_username)
+        if update.message.text.lower() == "what is my name?":
+            say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
+            bot.sendMessage(chat_id=chat_id, text=say_hello_username)
+        else:
+            bot.sendMessage(chat_id=chat_id, text=update.message.text)
     return "ok"

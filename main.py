1aa635421af8964819159d4b896aec90dfc20147
meirelon <nestelm@gmail.com>
2018-11-12 14:06:04 -0500

    new main

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def webhook(request):
 
         if update is None:
             return "not ok"
-
-        chat_id = update.message.chat.id
-        if update.message.text.lower() == "what is my name?":
-            say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
-            bot.sendMessage(chat_id=chat_id, text=say_hello_username)
-        else:
-            bot.sendMessage(chat_id=chat_id, text=update.message.text)
-            return "ok"
+        try:
+            chat_id = update.message.chat.id
+            if update.message.text.lower() == "what is my name?":
+                say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
+                bot.sendMessage(chat_id=chat_id, text=say_hello_username)
+            else:
+                bot.sendMessage(chat_id=chat_id, text=update.message.text)
+                return "ok"

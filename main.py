e292d9fc00ffec3b7527287c7b91f7c21b9c757a
meirelon <nestelm@gmail.com>
2018-11-12 13:53:51 -0500

    silent error

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import telegram
 def webhook(request):
     bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
     if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        update = telegram.Update.de_json(request.get_json(force=True,
+                                                          silent=True,
+                                                          cache=True), bot)
 
-        if isinstance(update, None):
+        if isinstance(update, NoneType):
             return "not ok"
+
+        chat_id = update.message.chat.id
+        if update.message.text.lower() == "what is my name?":
+            say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
+            bot.sendMessage(chat_id=chat_id, text=say_hello_username)
         else:
-            chat_id = update.message.chat.id
-            if update.message.text.lower() == "what is my name?":
-                say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
-                bot.sendMessage(chat_id=chat_id, text=say_hello_username)
-            else:
-                bot.sendMessage(chat_id=chat_id, text=update.message.text)
-                return "ok"
+            bot.sendMessage(chat_id=chat_id, text=update.message.text)
+            return "ok"

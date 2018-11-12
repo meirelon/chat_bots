7c3db652574423399591ea02e71481e02839a55a
meirelon <nestelm@gmail.com>
2018-11-12 13:40:34 -0500

    fixing error

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def webhook(request):
     bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-        try:
-            if update.message.text.lower() == "what is my name?":
-                say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
-                bot.sendMessage(chat_id=chat_id, text=say_hello_username)
-            else:
-                bot.sendMessage(chat_id=chat_id, text=update.message.text)
-                return "ok"
-        except:
+
+        if update is None:
             return "not ok"
+
+        chat_id = update.message.chat.id
+        if update.message.text.lower() == "what is my name?":
+            say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
+            bot.sendMessage(chat_id=chat_id, text=say_hello_username)
+        else:
+            bot.sendMessage(chat_id=chat_id, text=update.message.text)
+            return "ok"

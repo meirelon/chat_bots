ff10af1d0363646ced8ee0821d64aa1b8e4c988d
meirelon <nestelm@gmail.com>
2018-11-12 11:57:13 -0500

    changing response

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
 import telegram
+from telegram.ext import Updater, CommandHandler
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
 
 def webhook(request):
     bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
+        say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
         # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        # bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        bot.sendMessage(chat_id=chat_id, text=say_hello_username)
     return "ok"

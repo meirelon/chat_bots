baed1d061e4016cfd6345df621cc3223c03c6644
meirelon <nestelm@gmail.com>
2018-11-12 13:35:50 -0500

    adding try if not message

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ def webhook(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        if update.message.text.lower() == "what is my name?":
-            say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
-            bot.sendMessage(chat_id=chat_id, text=say_hello_username)
-        else:
-            bot.sendMessage(chat_id=chat_id, text=update.message.text)
-    return "ok"
+        try:
+            if update.message.text.lower() == "what is my name?":
+                say_hello_username = 'Hello {}'.format(update.message.from_user.first_name)
+                bot.sendMessage(chat_id=chat_id, text=say_hello_username)
+            else:
+                bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        return "ok"
+    except:
+        return "not ok"

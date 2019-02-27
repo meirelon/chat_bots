d76eb2b486e92ed5cd3eb5171437ac35b939f54d
meirelon <nestelm@gmail.com>
2019-02-27 09:31:31 -0500

    now use function

--- utils.py
+++ utils.py
@@ -25,5 +25,4 @@ def get_image_emotion(photo_link, image, instance, zone):
     docker_cmd_formatted = docker_cmd.format(photo_link=photo_link, image=image)
     gcloud_cmd = """gcloud compute ssh {instance} --zone {zone} -- """
     cli = gcloud_cmd.format(instance=instance, zone=zone) + docker_cmd_formatted
-    # return str(os.popen(cli).read().strip())
-    return "neutral"
+    return str(os.popen(cli).read().strip())

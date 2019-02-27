500191cc49d2f83b3b4a70947e9e86893811f74f
meirelon <nestelm@gmail.com>
2019-02-27 09:13:04 -0500

    test neutral

--- image_util.py
+++ image_util.py
@@ -1,8 +1,10 @@
 import os
+from subprocess import check_output
 
 def get_image_emotion(photo_link, image, instance, zone):
     docker_cmd = "'docker run -e photo_link={photo_link} -e detection_type=face {image}'"
     docker_cmd_formatted = docker_cmd.format(photo_link=photo_link, image=image)
     gcloud_cmd = """gcloud compute ssh {instance} --zone {zone} -- """
     cli = gcloud_cmd.format(instance=instance, zone=zone) + docker_cmd_formatted
-    return str(os.popen(cli).read().strip())
+    # return str(os.popen(cli).read().strip())
+    return "neutral"

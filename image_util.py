338c143f3211996d25f3b1cb72508324d558e8f7
meirelon <nestelm@gmail.com>
2019-02-27 08:54:06 -0500

    test with strip

--- image_util.py
+++ image_util.py
@@ -5,8 +5,4 @@ def get_image_emotion(photo_link, image, instance, zone):
     docker_cmd_formatted = docker_cmd.format(photo_link=photo_link, image=image)
     gcloud_cmd = """gcloud compute ssh {instance} --zone {zone} -- """
     cli = gcloud_cmd.format(instance=instance, zone=zone) + docker_cmd_formatted
-    try:
-        response = os.popen(cli).read().replace("\n", "")
-    except:
-        response = "neutral"
-    return response
+    return str(os.popen(cli).read().strip())
